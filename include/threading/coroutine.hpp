4bcee27784daabc439ebb4bdd0dffff9fa849603
lnikon <bejanyan.vahag@protonmail.com>
2021-10-16 23:39:41 +0400

    Fix formatting

--- include/threading/coroutine.hpp
+++ include/threading/coroutine.hpp
@@ -11,7 +11,7 @@ namespace threading
 {
 
 typedef struct coro_t_ coro_t;
-typedef int (*coro_function_t)(coro_t *coro);
+typedef int (*coro_function_t)(coro_t* coro);
 
 /*
  * Coroutine handler.
@@ -19,23 +19,23 @@ typedef int (*coro_function_t)(coro_t *coro);
 struct coro_t_
 {
     coro_function_t function;
-    ucontext_t      suspend_context;
-    ucontext_t      resume_context;
-    int             yield_value;
-    bool            is_coro_finished;
+    ucontext_t suspend_context;
+    ucontext_t resume_context;
+    int yield_value;
+    bool is_coro_finished;
 };
 
 /*
  * Coroutine APIs for users/
  */
-coro_t *coro_new(coro_function_t function);
+coro_t* coro_new(coro_function_t function);
 
-int coro_resume(coro_t *coro);
+int coro_resume(coro_t* coro);
 
-void coro_yield(coro_t *coro, int value);
+void coro_yield(coro_t* coro, int value);
 
-void coro_free(coro_t *coro);
+void coro_free(coro_t* coro);
 
 } // namespace threading
 
-#endif //COROUTINES_COROUTINES_HPP
+#endif // COROUTINES_COROUTINES_HPP
